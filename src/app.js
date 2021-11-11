c62b742ebb5aac78ea1613be5797307957792195
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2021-11-11 20:17:17 +0200

    feat: initial setup

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false, executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' });
+  const page = await browser.newPage();
+  await page.goto('https://ru.wargaming.net/id/signin/');
+
+await page.waitForSelector('[name=login][type=email]');
+
+try {
+    await page.$eval('[name=login][type=email]', el => el.value = 'test@example.com');
+    await page.$eval('[name=password][type=password]', el => el.value = 'test@example.com');
+} catch(e) {
+    console.error(e);
+}
+
+    console.log('before close');
+
+  await browser.close();
+})();
