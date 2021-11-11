aca87ac98b404f48e9c26f0adb1ec5e295acf0be
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2021-11-11 20:41:16 +0200

    style: add linters

--- src/app.js
+++ src/app.js
@@ -1,20 +1,27 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false, executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' });
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  });
   const page = await browser.newPage();
   await page.goto('https://ru.wargaming.net/id/signin/');
 
-await page.waitForSelector('[name=login][type=email]');
+  await page.waitForSelector('[name=login][type=email]');
 
-try {
-    await page.$eval('[name=login][type=email]', el => el.value = 'test@example.com');
-    await page.$eval('[name=password][type=password]', el => el.value = 'test@example.com');
-} catch(e) {
+  try {
+    await page.$eval('[name=login][type=email]', (el) => {
+      el.value = 'test@example.com';
+    });
+    await page.$eval('[name=password][type=password]', (el) => {
+      el.value = 'test@example.com';
+    });
+  } catch (e) {
     console.error(e);
-}
+  }
 
-    console.log('before close');
+  console.info('before close');
 
   await browser.close();
 })();
