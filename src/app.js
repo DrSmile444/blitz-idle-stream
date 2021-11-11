878036a83e56cae384195864f078fac4ed4fdf96
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2021-11-11 20:48:53 +0200

    feat: add submit click

--- src/app.js
+++ src/app.js
@@ -1,25 +1,41 @@
 const puppeteer = require('puppeteer');
 
+const SELECTORS = {
+  email: '[name=login][type=email]',
+  password: '[name=password][type=password]',
+  submit: 'button[type="submit"]',
+};
+
+const PAGES = {
+  login: 'https://ru.wargaming.net/id/signin/',
+};
+
 (async () => {
+  /**
+   * Start chrome
+   */
   const browser = await puppeteer.launch({
     headless: false,
     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
   });
+
+  /**
+   * Open login
+   */
   const page = await browser.newPage();
-  await page.goto('https://ru.wargaming.net/id/signin/');
+  await page.goto(PAGES.login);
 
-  await page.waitForSelector('[name=login][type=email]');
-
-  try {
-    await page.$eval('[name=login][type=email]', (el) => {
-      el.value = 'test@example.com';
-    });
-    await page.$eval('[name=password][type=password]', (el) => {
-      el.value = 'test@example.com';
-    });
-  } catch (e) {
-    console.error(e);
-  }
+  /**
+   * Fill login form and submit
+   */
+  await page.waitForSelector(SELECTORS.email);
+  await page.$eval(SELECTORS.email, (el) => {
+    el.value = 'test@example.com';
+  });
+  await page.$eval(SELECTORS.password, (el) => {
+    el.value = 'test@example.com';
+  });
+  await page.click(SELECTORS.submit);
 
   console.info('before close');
 
