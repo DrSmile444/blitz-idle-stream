b16230f6a0f958a95d035e627afc85d5b8c2edd5
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2021-11-12 08:10:01 +0200

    feat: support multiple accounts and pages

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
 const puppeteer = require('puppeteer');
-require('dotenv').config();
 
-const LOCAL_ENV = {
-  email: process.env.EMAIL,
-  password: process.env.PASSWORD,
-  streamUrl: process.env.STREAM_URL,
-};
+const config = require('../config.json');
 
 const SELECTORS = {
   email: '[name=login][type=email]',
@@ -15,50 +10,57 @@ const SELECTORS = {
 
 const PAGES = {
   login: 'https://ru.wargaming.net/id/signin/',
-  stream: LOCAL_ENV.streamUrl,
+  streams: config.streams,
 };
 
 (async () => {
-  /**
-   * Start chrome
-   */
-  const browser = await puppeteer.launch({
-    headless: false,
-    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  config.users.forEach((user) => {
+    PAGES.streams.forEach(async (streamUrl) => {
+      /**
+       * Start chrome
+       */
+      const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+      });
+
+      /**
+       * Open login
+       */
+      const page = await browser.newPage();
+      // Configure the navigation timeout
+      await page.setDefaultNavigationTimeout(200000);
+      await page.goto(PAGES.login);
+
+      /**
+       * Fill login form and submit
+       */
+      await page.waitForSelector(SELECTORS.email);
+      await page.$eval(
+        SELECTORS.email,
+        (el, localUser) => {
+          console.info('before close');
+          el.value = localUser.login;
+        },
+        user,
+      );
+      await page.$eval(
+        SELECTORS.password,
+        (el, localUser) => {
+          el.value = localUser.password;
+        },
+        user,
+      );
+
+      await page.click(SELECTORS.submit);
+      await page.waitForNavigation({ waitUntil: 'networkidle2' });
+
+      /**
+       * Open stream url
+       */
+      await page.goto(streamUrl);
+
+      console.info('Page launched!', user.email, streamUrl);
+    });
   });
-
-  /**
-   * Open login
-   */
-  const page = await browser.newPage();
-  await page.goto(PAGES.login);
-
-  /**
-   * Fill login form and submit
-   */
-  await page.waitForSelector(SELECTORS.email);
-  await page.$eval(
-    SELECTORS.email,
-    (el, local) => {
-      console.info('before close');
-      el.value = local.email;
-    },
-    LOCAL_ENV,
-  );
-  await page.$eval(
-    SELECTORS.password,
-    (el, local) => {
-      el.value = local.password;
-    },
-    LOCAL_ENV,
-  );
-
-  await page.click(SELECTORS.submit);
-  await page.waitForNavigation({ waitUntil: 'networkidle2' });
-
-  await page.goto(PAGES.stream);
-
-  console.info('before close');
-
-  // await browser.close();
 })();
